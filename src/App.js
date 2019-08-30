e616a7c45b5cba27724a53d640f0ee6d4e5ed872
Zayn Jarvis <zaynjarvis@gmail.com>
2019-08-30 22:38:38 +0800

    :trollface: cleanup

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 			<div className="wrapper">
 				{id ? (
 					<div className="barcode-page">
-						{/* <h2 className="title id-wrapper">
+						<h2 className="title id-wrapper">
 							{id}
 							<span
 								className="delete"
@@ -27,7 +27,7 @@ function App() {
 							>
 								x
 							</span>
-						</h2> */}
+						</h2>
 						<div className="barcode">
 							{Code39(id)
 								.toString()
